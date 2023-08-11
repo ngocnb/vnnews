827a90635e3c96caf5d8b1975b28b4e8afd3753a
Nguyen Bao Ngoc <ngocnguyenbao2707@gmail.com>
2023-08-11 11:46:51 +0700

    #22 use infyom to generate api scaffold for posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    public $table = 'posts';
+
+    public $fillable = [
+        'title',
+        'description',
+        'link',
+        'source',
+        'content',
+        'score_time',
+        'score_click',
+        'score_like',
+        'score_hot',
+        'is_new'
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'description' => 'string',
+        'link' => 'string',
+        'source' => 'boolean',
+        'content' => 'string',
+        'is_new' => 'boolean'
+    ];
+
+    public static array $rules = [
+        'title' => 'required|string|max:255',
+        'description' => 'required|string|max:500',
+        'link' => 'required|string|max:1000',
+        'source' => 'required|boolean',
+        'content' => 'nullable|string|max:65535',
+        'score_time' => 'nullable',
+        'score_click' => 'nullable',
+        'score_like' => 'nullable',
+        'score_hot' => 'nullable',
+        'is_new' => 'required|boolean',
+        'created_at' => 'nullable',
+        'updated_at' => 'nullable'
+    ];
+
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('posts', App\Http\Controllers\API\PostAPIController::class)
+    ->except(['create', 'edit']);
