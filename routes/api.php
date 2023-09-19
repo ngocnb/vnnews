d87280a1917473bfc5603bcfccfccd93e2ec0966
Nguyễn Đình Quang Anh <quanganh12a5kc@gmail.com>
2023-09-19 13:09:33 +0700

    Add Route Login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\PostAPIController;
+use App\Http\Controllers\API\UserAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\PostAPIController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,12 +14,11 @@ use App\Http\Controllers\API\PostAPIController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 Route::resource('posts', App\Http\Controllers\API\PostAPIController::class)
     ->except(['create', 'edit']);
@@ -31,3 +32,8 @@ Route::resource('tags', App\Http\Controllers\API\TagAPIController::class)
 Route::post('/loadData', [PostAPIController::class, 'loadData']);
 Route::get('/getNewsById/{id}', [PostAPIController::class, 'getNewsById']);
 Route::get('/search/{input?}', [PostAPIController::class, 'search']);
+Route::post('/login', [UserAPIController::class, 'login']);
+
+Route::middleware('api_token')->group(function () {
+    Route::Post('getUser', [UserAPIController::class, 'getUser']);
+});
